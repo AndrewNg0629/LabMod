252d1a0aac9b48e77c624dc314cce4e2a842a704
AndrewNg0629 <AndrewNg0629@outlook.com>
2026-07-05 08:02:10 +0800

    Built config prototype.

--- src/main/java/top/aenp/labmod/LabMod.java
+++ src/main/java/top/aenp/labmod/LabMod.java
@@ -1,6 +1,9 @@
 package top.aenp.labmod;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.gson.*;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.JsonOps;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.loader.api.FabricLoader;
@@ -13,10 +16,13 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import top.aenp.labmod.config.v2.prototype.ModConfig;
 import top.aenp.labmod.item.DebuggerItem;
 import top.aenp.labmod.network.v2.prototype.*;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class LabMod implements ModInitializer {
@@ -29,23 +35,119 @@ public class LabMod implements ModInitializer {
 	public static final Identifier TEST_CHANNEL = Identifier.of("mw", "test0");
 
 	public static final Item DEBUGGER = registerItem("debugger", new DebuggerItem(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
+	public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+	public static final ModConfig TEST_CONFIG = new ModConfig(
+			true,
+			false,
+			true,
+			new ModConfig.ModIdValidationConfig(
+					false,
+					List.of(),
+					List.of()
+			),
+			new ModConfig.ToggleTweaksSection1(
+					false,
+					false,
+					false,
+					false,
+					false,
+					false,
+					false,
+					false,
+					false,
+					false,
+					false,
+					false,
+					false,
+					false,
+					false,
+					false
+			),
+			new ModConfig.ToggleTweaksSection2(
+					false,
+					false,
+					false
+			),
+			new ModConfig.ValueTweaks(
+					new ModConfig.ValueTweaks.FireballAutoDiscarding(
+							false,
+							200
+					),
+					new ModConfig.ValueTweaks.StuffedShulkerBoxStacking(
+							false,
+							1
+					),
+					new ModConfig.ValueTweaks.ShulkerBoxNesting(
+							true,
+							2
+					),
+					new ModConfig.ValueTweaks.WardenAttributesControl(
+							false,
+							500.0,
+							1.0,
+							30.0,
+							1.5,
+							1.2,
+							18
+					),
+					new ModConfig.ValueTweaks.WardenSonicBoomControl(
+							false,
+							true,
+							10.0,
+							1.0,
+							34
+					),
+					new ModConfig.ValueTweaks.PlayerDeathItemProtection(
+							false,
+							12000,
+							false,
+							false
+					)
+			),
+			new ModConfig.ItemEditorConfig(
+					false,
+					List.of()
+			)
+	);
+
 
 	@Override
 	public void onInitialize() {
 		MythicNetwork.LOGIN_S2C_CODECS.put(TestLoginS2CPayload.ID, TestLoginS2CPayload.CODEC);
 		MythicNetwork.LOGIN_C2S_CODECS.put(TestLoginC2SPayload.ID, TestLoginC2SPayload.CODEC);
-		MythicNetwork.LOGIN_C2S_DECODERS.put(TestLoginC2SPayload.ID, TestLoginC2SPayload::new);
 		MythicNetwork.CUSTOM_PAYLOAD_CODECS.put(TestCommonS2CPayload.ID.id(), TestCommonS2CPayload.CODEC);
 		MythicNetwork.CUSTOM_PAYLOAD_CODECS.put(TestPlayC2SPayload.ID.id(), TestPlayC2SPayload.CODEC);
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.OPERATOR).register((itemGroup) -> itemGroup.add(DEBUGGER));
-		try {
-			Class.forName("net.minecraft.network.packet.s2c.common.CustomPayloadS2CPacket");
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		//JsonElement result = ModConfig.CODEC.encodeStart(JsonOps.INSTANCE, TEST_CONFIG).result().orElseThrow();
+		//LabMod.LOGGER.info(LabMod.GSON.toJson(result));
+		String content = readResourceFile("test.json");
+		JsonElement jsonObject = JsonParser.parseString(content);
+		LOGGER.info(jsonObject.toString());
+		ModConfig config = ModConfig.CODEC.parse(JsonOps.INSTANCE, jsonObject).getOrThrow();
+		JsonElement encodedConfig = ModConfig.CODEC.encodeStart(JsonOps.INSTANCE, config).getOrThrow();
+		LOGGER.info(GSON.toJson(encodedConfig));
+
 	}
 
 	public static Item registerItem(String itemKey, Item item) {
 		return Registry.register(Registries.ITEM, Identifier.of(LabMod.MOD_ID, itemKey), item);
 	}
+
+	public static String readResourceFile(String name) {
+		try(InputStream inputStream = Objects.requireNonNull(LabMod.class.getClassLoader().getResourceAsStream(name));
+			Reader reader = new InputStreamReader(inputStream)) {
+			StringBuilder stringBuilder = new StringBuilder();
+			int readCharCount;
+			char[] charBuf = new char[128];
+			while ((readCharCount = reader.read(charBuf)) != -1) {
+				 stringBuilder.append(charBuf, 0, readCharCount);
+			}
+			return stringBuilder.toString();
+		} catch (IOException e) {
+			LabMod.LOGGER.error("Whoops!", e);
+			return "";
+		}
+	}
+
 }
---
+++ src/main/java/top/aenp/labmod/config/v2/prototype/ItemEditor.java
@@ -0,0 +1,99 @@
+package top.aenp.labmod.config.v2.prototype;
+
+import com.mojang.datafixers.util.Unit;
+import it.unimi.dsi.fastutil.objects.Reference2ObjectMap;
+import net.minecraft.component.ComponentType;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.util.Rarity;
+import org.jetbrains.annotations.NotNull;
+import top.aenp.labmod.ReflectionUtils;
+
+import java.util.HashMap;
+
+public class ItemEditor {
+    private final static HashMap<Item, ItemEditor> itemEditors = new HashMap<>();
+    private final Item carriedItem;
+    private final boolean itemDamageable;
+    private final int vanillaMaxStackSize;
+    private final int vanillaMaxDamage;
+    private final Rarity vanillaRarity;
+    private final boolean vanillaIsFood;
+    private final FoodComponent vanillaFoodComponent;
+    private final boolean vanillaFireResistance;
+    private final Item vanillaRecipeRemainder;
+    private int maxStackSize;
+    private int maxDamage;
+    private Rarity rarity;
+    private boolean isFood;
+    private FoodComponent foodComponent;
+    private boolean fireResistance;
+    private Item recipeRemainder;
+
+    private ItemEditor(@NotNull Item item) {
+        itemEditors.put(item, this);
+        this.carriedItem = item;
+        ReflectionUtils.separateItemComponents(this.carriedItem);
+        this.vanillaMaxStackSize = item.getComponents().getOrDefault(DataComponentTypes.MAX_STACK_SIZE, 1);
+        this.vanillaMaxDamage = item.getComponents().getOrDefault(DataComponentTypes.MAX_DAMAGE, 0);
+        this.vanillaRarity = item.getComponents().getOrDefault(DataComponentTypes.RARITY, Rarity.COMMON);
+        this.vanillaIsFood = item.getComponents().contains(DataComponentTypes.FOOD);
+        this.vanillaFoodComponent = item.getComponents().getOrDefault(DataComponentTypes.FOOD, null);
+        this.vanillaFireResistance = item.getComponents().contains(DataComponentTypes.FIRE_RESISTANT);
+        this.vanillaRecipeRemainder = item.getRecipeRemainder();
+        this.itemDamageable = this.carriedItem.getComponents().contains(DataComponentTypes.MAX_DAMAGE);
+        this.revertVanilla();
+
+    }
+
+    public static ItemEditor getInstance(@NotNull Item item) {
+        ItemEditor editor = itemEditors.get(item);
+        if (editor == null) {
+            editor = new ItemEditor(item);
+        }
+        return editor;
+    }
+
+    public void revertVanilla() {
+        this.maxStackSize = this.vanillaMaxStackSize;
+        this.maxDamage = this.vanillaMaxDamage;
+        this.rarity = this.vanillaRarity;
+        this.isFood = this.vanillaIsFood;
+        this.foodComponent = vanillaFoodComponent;
+        this.fireResistance = this.vanillaFireResistance;
+        this.recipeRemainder = this.vanillaRecipeRemainder;
+    }
+
+    public void loadFromConfigUnit(ModConfig.ItemEditorConfig.ItemEditorUnit unit) {
+        unit.maxStackSize().ifPresent(size -> this.maxStackSize = size);
+        unit.durability().ifPresent(durability -> this.maxDamage = durability);
+        unit.fireResistant().ifPresent(resistant -> this.fireResistance = resistant);
+        unit.rarity().ifPresent(rarity -> this.rarity = rarity);
+        unit.craftingRemainsEntry().ifPresent(remains -> this.recipeRemainder = remains.value());
+        unit.isFood().ifPresent(isFood -> this.isFood = isFood);
+        unit.wrappedFoodComponents().ifPresent(wrappedComponents -> this.foodComponent = wrappedComponents.createComponents());
+    }
+
+    public void apply() {
+        Reference2ObjectMap<ComponentType<?>, Object> underlyingMap = ReflectionUtils.getItemComponentsUnderlyingMap(this.carriedItem);
+        if (this.itemDamageable) {
+            underlyingMap.put(DataComponentTypes.MAX_DAMAGE, this.maxDamage);
+        } else {
+            underlyingMap.put(DataComponentTypes.MAX_STACK_SIZE, this.maxStackSize);
+        }
+        underlyingMap.put(DataComponentTypes.RARITY, this.rarity);
+        if (this.isFood) {
+            underlyingMap.remove(DataComponentTypes.FOOD);
+        } else {
+            underlyingMap.put(DataComponentTypes.FOOD, this.foodComponent);
+        }
+        if (this.fireResistance) {
+            underlyingMap.put(DataComponentTypes.FIRE_RESISTANT, Unit.INSTANCE);
+        } else {
+            underlyingMap.remove(DataComponentTypes.FIRE_RESISTANT);
+        }
+        ReflectionUtils.Item$recipeRemainder.setFieldValue(this.carriedItem, this.recipeRemainder.equals(Items.AIR) ? null : this.recipeRemainder);
+    }
+}
